e119299c97e3115730755fb0bb2b441a38b46d05
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2024-06-02 10:22:52 +0700

    Update config.php

--- API/config.php
+++ API/config.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost:3306";
-    $user = "ummuhafi_root";
-    $password = "Fayani12121976";
-    $database = "ummuhafi_presensi";
+    $server = "";
+    $user = "";
+    $password = "";
+    $database = "";
 
     $koneksi = mysqli_connect($server, $user, $password, $database);
     // if ($koneksi){
